1db068b9047bb42faffdcefbb1e06864fc8320b2
sathishsatti <31844545+sathishsatti@users.noreply.github.com>
2017-09-27 14:10:54 +0530

    This

--- src/main/java/controller/IndexController.java
+++ src/main/java/controller/IndexController.java
@@ -1,42 +1,11 @@
 package controller;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
 import com.sun.glass.ui.View;
-
-import org.springframework.stereotype;
-import org.springframework.beans.factory.annotation.Autowired;
 
 @Controller
 public class IndexController
 {
-		@Autowired
-		UserDAoImpl userDaoImpl;
-		@RequestMapping("/")
-		public String index()
+	
+	
 
-	{
-			return "index";
-	}
-		@requestmapping("/register")
-		public string go To register()
-		{
 }
-		return "register";
-		@requestmapping(value="/saveRegister".Method=RequestMethod.POST)
-		public Model and viewsave register
-		(@modelAtrribute("user")User user)Binding result
-		{
-			Model and View MV=new model and view();
-			if (res.hasErrors())
-			{
-				MV.set viewName("register");
-			}
-			else
-			{
-				user.setrole("ROLE_user");
-				UserDaoImpl.insert User(user);
-				MV.setviewname("index");
-				return MV;	
-			}
-		}
